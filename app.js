497028d9017bb581dec6a5a8b4ff76c1ce53e3b5
akami2733 <akami2733@gmail.com>
2023-11-21 23:57:20 +0900

    adding color to vertex

--- app.js
+++ app.js
@@ -2,23 +2,29 @@ var vertexShaderText = [
     'precision mediump float;',
     '',
     'attribute vec2 vertPosition;',
+    'attribute vec3 vertColor;',
+    'varying vec3 fragColor;',
+    'uniform float screenWidth;',
     '',
     'void main() {',
+    'fragColor = vertColor;',
     'gl_Position = vec4(vertPosition, 0.0, 1.0);',
     '}'
 ].join('\n')
 
 var fragmentShaderText = [
     'precision mediump float;',
+    'varying vec3 fragColor;',
     '',
     'void main() {',
-    'gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);',
+    'gl_FragColor = vec4(fragColor, 1.0);',
     '}'
 ].join('\n')
 
 var InitDemo = function() {
-    console.log('333')
     var canvas = document.getElementById('game_surface')
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
     var gl = canvas.getContext('webgl')
     if (!gl) {
         gl = canvas.getContext('experimental-webgl')
@@ -66,25 +72,35 @@ var InitDemo = function() {
     }
 
     var triangleVertices = [
-        0.0, 0.5,
-        -0.5, -0.5,
-        0.5, -0.5
+        0.0, 0.5, 1.0, 0.0, 0.0,
+        -0.5, -0.5, 0.0, 1.0, 0.0,
+        0.5, -0.5, 0.0, 0.0, 1.0
     ]
     var triangleVertexBufferObject = gl.createBuffer()
     gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject)
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW)
 
     var positionAttribLocation = gl.getAttribLocation(program, 'vertPosition')
+    var colorAttribLocation = gl.getAttribLocation(program, 'vertColor')
     gl.vertexAttribPointer(
         positionAttribLocation,
         2,
         gl.FLOAT,
         gl.FALSE,
-        2 * Float32Array.BYTES_PER_ELEMENT,
+        5 * Float32Array.BYTES_PER_ELEMENT,
         0
+    )
+    gl.vertexAttribPointer(
+        colorAttribLocation,
+        3,
+        gl.FLOAT,
+        gl.FALSE,
+        5 * Float32Array.BYTES_PER_ELEMENT,
+        2 * Float32Array.BYTES_PER_ELEMENT
     )
 
     gl.enableVertexAttribArray(positionAttribLocation)
+    gl.enableVertexAttribArray(colorAttribLocation)
     
     gl.useProgram(program)
     gl.drawArrays(gl.TRIANGLES, 0, 3)
